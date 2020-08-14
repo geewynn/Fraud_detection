e60b45234c38dc2cfbd9bb708d53d6f27f5b9c3d
Godwin <ekainugodwin@gmail.com>
2020-08-14 16:00:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,31 +12,33 @@ def predict(model, input_df):
 
 def run():
 
-    add_selectbox = st.sidebar.selectbox(
-    "How would you like to predict?",
-    ("Online"))
+#     add_selectbox = st.sidebar.selectbox(
+#     "How would you like to predict?",
+#     ("Online"))
 
     st.sidebar.info('This app is created to predict patient hospital charges')
    
 
     st.title("Fraud Volume Prediction App")
 
-    if add_selectbox == 'Online':
+    #if add_selectbox == 'Online':add_selectbox = st.sidebar.selectbox(
+    # "How would you like to predict?",
+    # ("Online",))
 
-        Year = st.number_input('Year', min_value=1, max_value=3000, value=2013)
-        Channel = st.selectbox('Channel', ['ATM', 'Internet Banking', 'Mobile', 'POS', 'Web', 'eCommerce' ])
-        Value = st.number_input('Value', min_value=10, max_value=999999999999, value=25600750)
+    Year = st.number_input('Year', min_value=1, max_value=3000, value=2013)
+    Channel = st.selectbox('Channel', ['ATM', 'Internet Banking', 'Mobile', 'POS', 'Web', 'eCommerce' ])
+    Value = st.number_input('Value', min_value=10, max_value=999999999999, value=25600750)
 
-        output=""
+    output=""
 
-        input_dict = {'Year' : Year, 'Channel' : Channel, 'Value' : Value}
-        input_df = pd.DataFrame([input_dict])
+    input_dict = {'Year' : Year, 'Channel' : Channel, 'Value' : Value}
+    input_df = pd.DataFrame([input_dict])
 
-        if st.button("Predict"):
-            output = predict(model=model, input_df=input_df)
-            output = '$' + str(output)
+    if st.button("Predict"):
+        output = predict(model=model, input_df=input_df)
+        output = '$' + str(output)
 
-        st.success('The predicted fraud value is {}'.format(output))
+    st.success('The predicted fraud value is {}'.format(output))
 
 
 if __name__ == '__main__':
