6fc842f9cb63396fcc9bb1c947f3c3d45b52e8fd
Godwin <ekainugodwin@gmail.com>
2020-11-28 17:40:52 +0100

    new

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def run():
     st.success('The forcasted fraud volume is {}'.format(output))
 
     # read data
-    data = pd.read_csv('fraud data - Sheet1.csv')
+    data = pd.read_csv('fraud_data_sheet.csv')
     data['Value'] = data['Value'].str.replace(',', '').astype(float)
     data['Volume'] = data['Volume'].str.replace(',', '').astype(float)
 
