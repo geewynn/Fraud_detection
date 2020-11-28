d604e4a255936a627b8e046d03244a678785cfb2
Godwin <ekainugodwin@gmail.com>
2020-11-28 13:56:20 +0100

    added chart and table view

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from pycaret.regression import load_model, predict_model
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 model = load_model('deployment_1')
 
@@ -12,9 +13,9 @@ def predict(model, input_df):
 
 def run():
     
-    st.sidebar.info('This app is created to predict fraud values in web channels')
+    st.sidebar.info('This app is created to forecast fraud values in web channels')
 
-    st.title("Fraud Volume Prediction App")
+    st.title("Fraud Volume Forecast App")
 
     Year = st.number_input('Year', min_value=1, max_value=3000, value=2013)
     Channel = st.selectbox('Channel', ['ATM', 'Internet Banking', 'Mobile', 'POS', 'Web', 'eCommerce' ])
@@ -25,11 +26,53 @@ def run():
     input_dict = {'Year' : Year, 'Channel' : Channel, 'Value' : Value}
     input_df = pd.DataFrame([input_dict])
 
-    if st.button("Predict"):
+    if st.button("Forecast"):
         output = predict(model=model, input_df=input_df)
         output = '$' + str(output)
 
-    st.success('The predicted fraud volume is {}'.format(output))
+    st.success('The forcasted fraud volume is {}'.format(output))
+
+    # read data
+    data = pd.read_csv('/home/godwin/Fraud_detection/fraud data - Sheet1.csv')
+    data['Value'] = data['Value'].str.replace(',', '').astype(float)
+    data['Volume'] = data['Volume'].str.replace(',', '').astype(float)
+
+    #for year in Year:
+    data = data[data['Year'] == 2016]
+    fig1, ax1 = plt.subplots()
+    ax1.pie(data['Value'], labels=data['Channel'], autopct='%1.1f%%', startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    #plt.show()
+    st.pyplot()
+    st.subheader('Recommendations and Implementation')
+    st.table(pd.DataFrame({
+        'Target':['User', 'User','User','User','User', 'Service Provider', 'Service Provider', 'Service Provider', 'Service Provider'],
+
+        'Vulnerability': ['Over the Air (OTA) transmission between phone and Point of Sale(POS) (NFC reader)',
+                        'Inadvertent installation of malicious software on a mobile phone by the user',
+                      'Absence of two- factor authentication ', 'Changing or replacing the mobile phone', 'Smartphone internet and geolocation capabilities','POS system accepts OTA transmissions',
+                      'POS devices are installed at merchant premises', 'Lack of Digital Rights Management (DRM) on the mobile device', 'Weakness of Global System for mobile communication(GSM) encryption for OTA transmission; SMS data in clear text on mobile network'],
+        
+        'Threat': ['Interception of Traffic', 'Downloaded applications intercept of authentication data',
+                   'User masquerading', 'Configuration and setup complecity', 'Malware on the mobile device; poor data protection controls at merchant/payment processor', 'Malicious party floods POS system with meaningless requests',
+                   'Masquerade attacks; tampering with POS', 'Mobile device user illegally distributes content; e.g ringtone, video, games, etc', 'Message modification, a replay of transactions, evasion of fraud controls'],
+        
+        'Risk': ['Identity theft, Information disclosure, replay attacks', 'fraudulent transactions, procider liabilities', 'fraudulent transactions, procider liabilities',
+                'Reduced adoption of the technology, "security by obscurity"','Data disclosure and privacy infringement; profiling of user behaviour','Denial of Service (DoS)',
+                'Theft of service, replay, message modification', 'Theft of content, Digital piracy, a risk to the provider for digital rights infringement, loss of revenue to the content provider of merchant',
+                'Theft of Service or content, loss of revenue, illegal transfer of funds'],
+        
+        'Proposed Action': ['Trusted Platform Module(TPM), secure protocols, encryption', 'Authentication of both user (PIN) and application ( digital signature by trusted third-party), TPM', 'Two-Factor authentication',
+                'A simplified user interface, security parameters in TPM set by a trusted party', 'User control of geolocation features, cryptographically supported privacy, trusted platform module,vetted authorization and accounting',
+                'Request filtering at reader based on mobile device0- reader relative geometry', 'POS vendor vetting, message authenticators, vetted authorization and accounting', 'Trusted Platform Module (TPM) design, cryptographically supported DRM',
+                'Strong Cryptographic protocols, SMS message authenticators, encryption']
+        
+    }))
+    st.subheader('Projection')
+    st.write('1. If decision makers do not address fears related to online payment, customers will be discouraged from making online transaction with these businesses. This may reduce revenue and eventually lead to business shut down.')
+
+    st.write('2. “Protecting websites against hackers will minimize customers’ fears of e-payment which can increases revenue and business growth.”')
+    st.markdown('If decision makers allow their business site to be vulnerable to hackers it discourages customers from making transaction on their sites which may reduce revenue and eventually lead to business shut down.')
 
 if __name__ == '__main__':
     run()
